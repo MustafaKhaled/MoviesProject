699da8b76cad9d047f11a089e8308d46b7a53cda
Mustafa Khaled Mustafa Mohamed <mustafakhaled290@gmail.com>
2020-05-15 00:53:21 +0200

    initialize dagger AppComponent

--- app/src/main/java/com/werd/khaleds/moviesprojectswvlchallenge/MyApplication.kt
+++ app/src/main/java/com/werd/khaleds/moviesprojectswvlchallenge/MyApplication.kt
@@ -1,6 +1,24 @@
 package com.werd.khaleds.moviesprojectswvlchallenge
 
 import android.app.Application
+import android.content.Context
+import com.avoid.covid19.di.component.ApplicationComponent
+import com.avoid.covid19.di.component.DaggerApplicationComponent
+import com.avoid.covid19.di.module.ContextModule
+import com.avoid.covid19.di.module.OkHttpModule
+import com.avoid.covid19.di.module.RetrofitModule
 
 class MyApplication: Application() {
+    companion object {
+        lateinit var applicationComponent: ApplicationComponent
+        lateinit  var appContext: Context
+    }
+    override fun onCreate() {
+        super.onCreate()
+        appContext = applicationContext
+        applicationComponent = DaggerApplicationComponent.builder().contextModule(ContextModule(applicationContext))
+            .okHttpModule(
+                OkHttpModule()
+            ).retrofitModule(RetrofitModule()).build()
+    }
 }
